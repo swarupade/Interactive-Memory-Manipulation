7ab9d95a337636ec95fa5cb9791a4d28a7a0ff4c
Steve Antony <steveantony5@gmail.com>
2018-09-25 16:35:05 -0600

    added default case

--- main.c
+++ main.c
@@ -16,9 +16,11 @@ void main()
 	
 	printf("\n********************* Welcome******************************\n");
 
+
+	printf("\nTo invoke the help function, type \"help\"\n");
+
 	while(1)
 	{
-		printf("\nTo invoke the help function, type \"help\"\n");
 		printf("\nEnter your option:\n1. To allocate memory - \"allocate\"\n2. To free memory - \"free\"\n3. To display allocated memory - \"display\"");
 		printf("\n4. To write memory - \"write\"\n5. To invert block \"invert\"\n6. To generate a pattern \"generate\"\n7. To Verify pattern \"verify\"");
 		printf("\n8. To Exit \"exit\"\n");
@@ -34,7 +36,7 @@ void main()
 		//look up table
 		table table_lookup[] =
 		{
-		//	{help,"help"},
+			{help,"help"},
 			{free_memory,"free"},
 			{allocate,"allocate"},
 			{display,"display"},
@@ -45,7 +47,7 @@ void main()
 			{NULL," "}
 		};
 
-
+		uint32_t flag = 0;//used to set when user enters an invalid command
 		for(int i=0;(table_lookup[i].func1)!=NULL ; i++)
 		{
 			//comparing the user option in the lookup table
@@ -53,10 +55,14 @@ void main()
 			{
 				//calling the function
 				(table_lookup[i].func1)();
+				flag = 1;//sets 1 if match is found in lookup table
 				break;
 
 			}
 		}
+		if(flag == 0)//enters if the user entered an invalid command
+		      printf("\nYou have entered an invalid command\nPlease type \"help\" for help\n");
+
 	}
 }
 			
